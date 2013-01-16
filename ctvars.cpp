a4a822e43cf589f8f3e35c17c3150b5dacd4d822
Leila Baghdadi <baghdadi@phenogenomics.ca>
2013-01-16 12:14:01 -0500

    added a few more attributes to read including filename prefix

--- ctvars.cpp
+++ ctvars.cpp
@@ -2,15 +2,19 @@
 ct_vars::ct_vars()
 {
   NumberOfFiles = 1;
-  NumberOfRows = 1000;
-  NumberOfColumns = 2000;
   Binning = "2x2";
   ImageRotation = 0.5;
   ImagePixelSize = 10.76;
-  Depth=16.0;
   Exposure = 950;
   RotationStep = 0.2;
   Rotation360 = "NO";
+  SourceVoltage = 49;
+  SourceCurrent = 200;
+  VerticalObjectPosition = 31.800;
+  ObjectToSource = 168.95;
+  CameraToSource = 282.96;
+  Filter = "Al 0.5 mm";
+  
  }
 ct_vars::~ct_vars()
 {
@@ -21,24 +25,41 @@ int readCtLogFile(const string & filename, ct_vars &ct)
  FILE * logFile;
  string currentLine;
  string  currentValue;
- std::ifstream file(filename.c_str());
- std::string line;
-
+ ifstream file(filename.c_str());
+ string line;
+ //bool found=false;
  while(!file.eof())
   {
-    std::getline(file, line);
-    
+    getline(file, line);
     currentLine = line.substr(0, line.find_last_of("=") + 1);
     currentValue = line.substr(line.find_last_of("=") + 1);
-    
-    if (currentLine.compare("Number of Files=") == 0) 
+/*
+    if (currentLine.compare("Filename Prefix=") == 0 && !found)
+     {
+       ct.setFilenamePrefix(currentValue); 
+       cout << currentValue.c_str() << endl;
+       found = true;
+     }
+*/  if (currentLine.compare("Filename Prefix=") == 0)
+      ct.setFilenamePrefix(currentValue.c_str());
+    else if (currentLine.compare("Number of Files=") == 0) 
       ct.setNumberOfFiles((long)atol(currentValue.c_str()));
+    else if (currentLine.compare("Source Voltage (kV)=") == 0)
+      ct.setSourceVoltage((long)atol(currentValue.c_str()));
+    else if (currentLine.compare("Source Current (uA)=") == 0)
+      ct.setSourceCurrent((long)atol(currentValue.c_str()));
     else if (currentLine.compare("Camera binning=") == 0) 
       ct.setBinning(currentValue.c_str()); 
     else if (currentLine.compare("Image Rotation=") == 0) 
-      ct.setImageRotation((float)atof(currentValue.c_str())); 
-    else if (currentLine.compare("Depth (bits)=") == 0) 
-      ct.setDepth((long)atol(currentValue.c_str()));
+      ct.setImageRotation((float)atof(currentValue.c_str()));     
+    else if (currentLine.compare("Object to Source (mm)=") == 0)
+      ct.setObjectToSource((float)atof(currentValue.c_str()));
+    else if (currentLine.compare("Camera to Source (mm)=") == 0)
+      ct.setCameraToSource((float)atof(currentValue.c_str()));
+    else if (currentLine.compare("Vertical Object Position (mm)") == 0)
+      ct.setVerticalObjectPosition((float)atof(currentValue.c_str()));
+    else if (currentLine.compare("Filter=") == 0) 
+      ct.setFilter(currentValue.c_str());
     else if (currentLine.compare("Exposure (ms)=") == 0) 
       ct.setExposure((long)atol(currentValue.c_str()));
     else if (currentLine.compare("Rotation Step (deg)=") == 0) 
@@ -51,11 +72,6 @@ int readCtLogFile(const string & filename, ct_vars &ct)
       ct.setNumberOfColumns((long)atol(currentValue.c_str()));
     else if (currentLine.compare("Pixel Size (um)=") == 0) 
       ct.setImagePixelSize((float)atof(currentValue.c_str()));
-    ///else
-     // {
-       //cout <<"Can not recognize this variable "<< currentValue << endl; 
-       //return 1;
-     //  }
-  }
+   }
  return 0;
 }
